702b430415a33ae955d14c6b9104617e444134ae
Mr-MiracL <lonel1nessgu@gmail.com>
2024-10-30 16:08:56 +0000

    Add to favourites logic completed; Dummy favourites page.

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -23,6 +23,7 @@ const HomePage = (props) => {
   const [nameFilter, setNameFilter] = useState("");
   const [genreFilter, setGenreFilter] = useState("0");
   const genreId = Number(genreFilter);
+  
 
   let displayedMovies = movies
     .filter((m) => {
@@ -31,7 +32,12 @@ const HomePage = (props) => {
     .filter((m) => {
       return genreId > 0 ? m.genre_ids.includes(genreId) : true;
     });
-
+    const addToFavorites = (movieId) => {
+      const updatedMovies = movies.map((m) =>
+        m.id === movieId ? { ...m, favorite: true } : m
+      );
+      setMovies(updatedMovies);
+    };
   const handleChange = (type, value) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
@@ -50,7 +56,7 @@ const HomePage = (props) => {
       genreFilter={genreFilter}
     />
         </Grid>
-        <MovieList movies={displayedMovies} />
+        <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
