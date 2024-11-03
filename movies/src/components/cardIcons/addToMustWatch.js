810b8b5661d607e1dfb5d88849c42be8c079dc97
Mr-MiracL <lonel1nessgu@gmail.com>
2024-11-03 00:48:08 +0000

    New button for upcoming movies page.

---
+++ movies/src/components/cardIcons/addToMustWatch.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@mui/material/IconButton";
+import PlaylistAddIcon from "@mui/icons-material/Favorite";
+
+const AddToMustWatchIcon = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
+  const handleAddToMustWatch = (e) => {
+    e.preventDefault();
+    context.addToMustWatch(movie);
+  };
+
+  return (
+    <IconButton aria-label="add to MustWatch" onClick={handleAddToMustWatch}>
+      <FavoriteIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default AddToMustWatchIcon;
